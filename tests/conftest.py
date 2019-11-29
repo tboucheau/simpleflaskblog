40b26c22b39345362a971e7fc6da0231c2bd334e
tboucheau <boucheau.tony@neuf.fr>
2019-11-29 17:18:23 +0100

    <build>(app): removed some useless files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import os
 import tempfile
-
 import pytest
-from flaskr import create_app
-from flaskr.db import get_db, init_db
+
+from src import create_app
+from src.db import get_db, init_db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,4 +1,4 @@
-from flaskr import create_app
+from src import create_app
 
 
 def test_config():
